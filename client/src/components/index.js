eb39bc35cf02f90b5c9904e8f91f1fd90d922b7b
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-05 20:49:37 +0330

    edit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,2 @@
-export {default as ChannelContainer , ChannelListContainer} from './';
+export {default as ChannelContainer} from './ChannelContaine';
+export {default as ChannelListContainer} from './ChannelListContainer';
