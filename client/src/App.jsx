0b36ee060d64fbd3d3317cbd09b1151a8bf4f048
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-05 20:10:03 +0330

    add import . get apikey

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
+const apikey = '5fyp6re2gxy3';
 const App = () => {
   return (
     <div>
