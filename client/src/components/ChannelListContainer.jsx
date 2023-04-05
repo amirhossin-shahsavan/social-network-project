9928cc1986f749e4c87ec64e493ed555174f23ad
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-06 01:52:33 +0330

    import .add sidbsr

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { ChannelList , useChatContext } from 'stream-chat-react';
+import { ChannelSearch , TeamChannelList , TeamChannelPreview } from 'stream-chat-react';
+import Cookies from 'universal-cookie'
+
+const SidBar = ()=>(
+  <div >
+
+  </div>
+)
 
 const ChannelListContainer = () => {
   return (
