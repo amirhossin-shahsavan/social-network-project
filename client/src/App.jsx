7e9ce9d2bf372d8f43797aa717cbef9be2479d93
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-04-05 20:27:45 +0330

    edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import {StreamChat} from 'stream-chat';
-import {Chat} from 'stream-chat-react';
+import {ChannelList, Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+import {ChannelListContainer , channelContainer } from './components';
 const apikey = '5fyp6re2gxy3';
+
+const client = StreamChat.getInstance(apikey);
+
+
+
 const App = () => {
   return (
-    <div>
-        <h1>Medical pager chat App</h1>
+    <div className='app__wrapper'>
+        <chat client={client} theme="team light">
+            <ChannelListCintainer
+            
+            />
+            <ChannelListCintainer
+            
+            />
+
+        </chat>
     </div>
   );
 }
